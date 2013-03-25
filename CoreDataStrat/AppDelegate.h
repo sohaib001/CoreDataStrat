41fad870e94cb34e3085ea6ac2c6ff6162d8a82f
Sohaib Muhammad <sohaib.muhammad@coeus-solutions.de>
2013-03-25 14:45:28 +0500

    Initial Commit

---
+++ CoreDataStrat/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CoreDataStrat
+//
+//  Created by Sohaib Muhammad on 25/03/2013.
+//  Copyright (c) 2013 coeus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
